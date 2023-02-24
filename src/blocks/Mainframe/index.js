060a184ad7564f77b636b0bd9448b023c926b68e
onlyphantom <samuel@outlook.sg>
2023-02-24 13:02:28 +0700

    add Head to Mainrame and some minor refactoring

--- src/blocks/Mainframe/index.js
+++ src/blocks/Mainframe/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Home from '@/icons/Home'
-import Themes from 'daisyui/src/colors/themes';
 
 import { MeContext } from '@/contexts/MeContext';
 
@@ -8,11 +7,21 @@ export const Mainframe = ({ children, data }) => {
 
   return (
     <MeContext.Provider value={data}>
+
+      <Head>
+        <title>{data.fullname} on Supertype Collective</title>
+        <meta name="description" content={`${data.fullname} (${data.short}) | ${data.affiliations.length > 0 && data.affiliations[0]['title'] + ' ' + data.affiliations[0]['position']} | Supertype Collective`} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+
       <main className={`max-w-7xl mx-auto gap-4 backdrop-blur-lg rounded drop-shadow-lg my-12 break-words bg-gradient-to-r from-amber-700 to-rose-900 mb-6 shadow-xl rounded-lg`} >
         <div className="grid grid-cols-12 items-center grid-flow gap-4 bg-black bg-opacity-30 rounded-lg px-4 lg:px-8">
           {children}
           <div className="footer col-span-12 rounded-lg border bg-opacity-30 bg-gray-200 p-6">
             {/* <!-- Footer content --> */}
+
+
           </div>
         </div>
       </main >
