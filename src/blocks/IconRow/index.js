145e190993a012759af3c2694e604c67f3a9455f
onlyphantom <samuel@outlook.sg>
2023-02-24 15:54:49 +0700

    fix missing key prop

--- src/blocks/IconRow/index.js
+++ src/blocks/IconRow/index.js
@@ -5,8 +5,8 @@ const IconRow = ({ tags, dark }) => {
 
         <div className="flex mb-4 space-x-16" >
             {tags.map((tag, index) => (
-                <div className="tooltip flex" data-tip={tag}>
-                    <Image key={index} src={
+                <div className="tooltip flex" key={index} data-tip={tag}>
+                    <Image src={
                         dark ? `/techicons/${tag}.png` : `/techicons/${tag}_inv.png`
                     } width={50} height={50} alt={tag} className="max-w-fit" />
                 </div>
