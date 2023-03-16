3b37331631590f3deb6f5abb08da1f3d46ee6911
onlyphantom <samuel@outlook.sg>
2023-03-16 13:48:46 +0700

    fix PageHead and add Umai

--- src/components/AddDevProfileCTA.js
+++ src/components/AddDevProfileCTA.js
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 const AddDevProfileCTA = () => {
     return (
-        <div class="flex items-center mt-8">
-            <div class="relative mx-auto w-96 overflow-hidden rounded-[16px] bg-gray-300 p-[1px] transition-all duration-300 ease-in-out bg-opacity-10">
-                <div class="relative rounded-[15px] bg-white p-6 bg-opacity-10">
-                    <div class="space-y-4 text-white">
+        <div className="flex items-center mt-8">
+            <div className="relative mx-auto w-96 overflow-hidden rounded-[16px] bg-gray-300 p-[1px] transition-all duration-300 ease-in-out bg-opacity-10">
+                <div className="relative rounded-[15px] bg-white p-6 bg-opacity-10">
+                    <div className="space-y-4 text-white">
                         <Image src="collaborators.svg" width={250} height={100} alt="supertype collective collaborators" />
-                        <p class="text-lg font-semibold">Add your Developer Profile</p>
-                        <p class="font-md text-gray-400 text-justify">Connect with collaborators and like-minded developers through impactful open source projects, and empower the local developer community in Indonesia in the process.</p>
+                        <p className="text-lg font-semibold">Add your Developer Profile</p>
+                        <p className="font-md text-gray-400 text-justify">Connect with collaborators and like-minded developers through impactful open source projects, and empower the local developer community in Indonesia in the process.</p>
                         <Link href="/enroll">
                             <button className="btn btn-md btn-info drop-shadow-lg mt-4">Join Collective</button>
                         </Link>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import Script from 'next/script'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script async defer data-website-id="d2ad8970-0b32-4ada-93a6-cfbd95030fda" src="https://umai.supertype.ai/umami.js" />
+      <Component {...pageProps} />
+    </>
+  )
 }
