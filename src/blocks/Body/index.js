edb2e283c2903414c21b7e970090af47c2da9e1f
onlyphantom <samuel@outlook.sg>
2023-03-25 18:54:09 +0700

    add contactcard

--- src/blocks/Body/index.js
+++ src/blocks/Body/index.js
@@ -4,6 +4,7 @@ import RepoCard from '@/components/RepoCard';
 import RepoTag from '@/components/RepoTag';
 
 import { MeContext } from '@/contexts/MeContext';
+import Link from 'next/link';
 
 
 const Body = ({ stack, affiliations, children }) => {
@@ -19,13 +20,13 @@ const Body = ({ stack, affiliations, children }) => {
 
             if (data['show_repo'] > 0) {
                 return (
-                    <div className="col-span-12 md:col-span-4 text-white my-8 mx-1">
+                    <div className="col-span-12 md:col-span-4 text-white my-8 mx-1 self-start">
                         <h3 className="text-lg uppercase font-semibold leading-normal mb-2 my-4">GitHub Projects</h3>
                         {
                             data['superinference']['stats']['top_repo_stars_forks'].map((repo, index) => {
                                 // show only up to data['show_repo'] repos
                                 if (index < data['show_repo']) {
-                                    return <RepoCard key={index} repo={repo} owner={data.github_handle} />
+                                    return <div key={index}><RepoCard repo={repo} owner={data.github_handle} /></div>
                                 }
                             })
                         }
@@ -46,7 +47,7 @@ const Body = ({ stack, affiliations, children }) => {
 
             else {
                 return (
-                    <div className="col-span-12 md:col-span-4 text-white my-8 mx-1">
+                    <div className="col-span-12 md:col-span-4 text-white my-8 mx-1 self-start">
                         <h3 className="text-lg uppercase font-semibold leading-normal mb-2 my-4">Open Source Contributions</h3>
                         {Object.keys(collaboration_count).map(e =>
                             <div key={e} className="inline-flex">
@@ -56,6 +57,60 @@ const Body = ({ stack, affiliations, children }) => {
                     </div>
                 )
             }
+        },
+        [data],
+    )
+
+    const renderContactCard = useCallback(
+        (data) => {
+            return (
+                <div className="col-span-12 md:col-span-4 text-white my-8 mx-1 self-start">
+                    <h3 className="text-lg uppercase font-semibold leading-normal mb-2 my-4">Personal Details</h3>
+                    {/* personal details card */}
+                    <div className="border-white border rounded-lg shadow-lg p-4">
+                        <div className="flex flex-row items-start">
+                            <div className="flex flex-col">
+                                <div className="avatar mr-2">
+                                    <div className="w-12 rounded-full">
+                                        <img src={
+                                            data['superinference']['profile']['avatar_url']
+                                        } />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col">
+                                <span className='leading-none font-semibold'>{data['fullname']}</span>
+                                {/* social links */}
+                                <div className="flex flex-row gap-x-2">
+                                    {
+                                        data['website'] &&
+                                        <Link href={data['website']} target="_blank" rel="noopener noreferrer" className='text-sm link-info hover:opacity-70'>
+                                            Website
+                                        </Link>
+                                    }
+                                    {
+                                        data['linkedin'] &&
+                                        <Link href={data['linkedin']} target="_blank" rel="noopener noreferrer" className='text-sm link-info hover:opacity-70'>
+                                            LinkedIn
+                                        </Link>
+                                    }
+                                    <Link href={`https:github.com/${data['github_handle']}`} target="_blank" rel="noopener noreferrer" className='text-sm link-info hover:opacity-70'>
+                                        GitHub
+                                    </Link>
+                                </div>
+                            </div>
+
+                        </div>
+                        <ul className="text-sm text-gray-400">
+                            <li>Remote / Online</li>
+                            <li>On-site: Jakarta, Indonesia</li>
+                        </ul>
+                        <p className="text-sm text-gray-400">
+                            {data['linkedin']}
+                        </p>
+                    </div>
+                </div>
+            )
         },
         [data],
     )
@@ -82,6 +137,7 @@ const Body = ({ stack, affiliations, children }) => {
                 {/* full width div for children */}
                 {renderGitHubProjects(data)}
                 {renderRepoTags(data)}
+                {renderContactCard(data)}
             </>
         )
     } else {
