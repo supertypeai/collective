a2261796509361244e64af34996d88b1edbc29fd
onlyphantom <samuel@outlook.sg>
2023-06-16 17:44:19 +0700

    add HireBasic forms

--- src/components/HireForm/HireBasic.js
+++ src/components/HireForm/HireBasic.js
@@ -1,8 +1,125 @@
-import React from 'react'
+import { useContext } from 'react'
+import { useForm, Controller } from "react-hook-form"
+import Select from "react-select";
 
-const HireBasic = () => {
+import { HireContext } from '@/contexts/HireContext'
+import { Field, Form, Input } from "@/blocks/Form"
+
+const HireBasic = ({ nextFormStep }) => {
+
+    const context = useContext(HireContext)
+    const [form, setForm] = context.f
+
+    const { register, control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            ...form
+        }, mode: "onSubmit"
+    });
+
+    const saveData = (data) => {
+        setForm({ ...form, ...data, superinference: superinference });
+        nextFormStep();
+    };
+
     return (
-        <div>Basic Questions here</div>
+        <Form onSubmit={handleSubmit(saveData)}>
+            <fieldset>
+                <legend>
+                    <h3 className="text-2xl font-bold">🧑‍💼 Connect me with Collective consultants</h3>
+                    <p className="text-sm">Help us find the right candidate(s) for your project.</p>
+                </legend>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <Field label="Your Name"
+                            error={errors?.name}
+                            hint="How should we address you?"
+                        >
+                            <Input
+                                {...register("name", { required: "Please provide a name to address you by" })}
+                                id="name"
+                                placeholder="Pamela Morgan Beesly"
+                            />
+                        </Field>
+                    </div>
+                    <div className="w-full md:w-1/2 px-3">
+                        <Field label="Company Email"
+                            error={errors?.email}
+                            hint="We&apos;ll use this to contact you."
+                        >
+                            <Input
+                                {...register("email", { required: "Please sign in with your GitHub account" })}
+                                id="email"
+                                placeholder="pam.beesly@dundermifflin.com"
+                            />
+                        </Field>
+                    </div>
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <Field label="Commitment Level"
+                            error={errors?.name}
+                            hint="The level of time commitment you require from the consultant(s)."
+                        >
+                            <Select
+                                id="commitment"
+                                className="text-black max-w-3xl"
+                                options={
+                                    [
+                                        { value: "full-time", label: "Full-time" },
+                                        { value: "part-time", label: "Part-time" },
+                                        { value: "hourly", label: "Hourly" },
+                                        { value: "undecided", label: "I'll decide later" },
+                                    ]
+                                }
+                                defaultValue={[
+                                    { value: "part-time", label: "Part-time" },
+                                ]}
+                                {...register("commitment")}
+                            />
+                        </Field>
+                    </div>
+                    <div className="w-full md:w-1/2 px-3">
+                        <Field label="Work Preference"
+                            error={errors?.name}
+                            hint="Open to working with your consultants remotely?"
+                        >
+                            <Select
+                                id="remoteness"
+                                className="text-black max-w-3xl"
+                                options={
+                                    [
+                                        { value: "remote", label: "Remote, anywhere in the world" },
+                                        { value: "mostly-remote", label: "Mostly remote" },
+                                        { value: "office", label: "Work from office" },
+                                        { value: "undecided", label: "I'll decide later" },
+                                    ]
+                                }
+                                defaultValue={[
+                                    { value: "mostly-remote", label: "Mostly remote" },
+                                ]}
+                                {...register("commitment")}
+                            />
+                        </Field>
+
+                    </div>
+                    <div className="w-full md:w-1/2 px-3">
+                        <Field label="🖊️ Description" error={errors?.description}>
+                            <textarea
+                                {...register("description", {
+                                    required: "Description is a required field",
+                                })}
+                                id="description"
+                                name="description"
+                                rows="4"
+                                required
+                                minLength="40"
+                                maxLength="500"
+                                placeholder="A good place to brief us about the project, the key job responsibities, your budget (if you have one), and any other details you think we should know."
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            />
+                        </Field>
+                    </div>
+                </div>
+            </fieldset>
+        </Form>
     )
 }
 
--- src/pages/hire/index.js
+++ src/pages/hire/index.js
@@ -2,28 +2,16 @@ import { useState } from "react"
 
 import { HireContext } from "@/contexts/HireContext";
 import { Mainframe } from "@/blocks/Mainframe";
-import FormBlock from "@/blocks/Form/FormBlock";
 
 import HireBasic from "@/components/HireForm/HireBasic";
 
 const HireForm = () => {
 
-    const [formStep, setFormStep] = useState(0);
-    const nextFormStep = () => setFormStep((currentStep) => currentStep + 1);
-    const prevFormStep = () => setFormStep((currentStep) => currentStep - 1);
-
     const [formState, setFormState] = useState({});
 
     return (
         <HireContext.Provider value={{ f: [formState, setFormState] }}>
-            <FormBlock
-                currentStep={formStep}
-                prevFormStep={prevFormStep}>
-                {formStep === 0 && (
-                    <HireBasic nextFormStep={nextFormStep} />
-                )}
-
-            </FormBlock>
+            <HireBasic />
         </HireContext.Provider>
     )
 }
@@ -31,7 +19,6 @@ const HireForm = () => {
 const Page = () => {
     return (
         <Mainframe title="Client Form | Supertype Collective">
-            <h1 className="text-4xl font-bold">Connect me with Collective talents</h1>
             <HireForm />
         </Mainframe>
     )
