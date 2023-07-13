15ec50d8f2f84aa8305a70046d4c2f12c4aff469
AurelliaChristie <aurelliachristie77@gmail.com>
2023-07-13 16:37:14 +0700

    minor UI enhancement

--- src/components/SessionManager/components/CurrentSessionCard.js
+++ src/components/SessionManager/components/CurrentSessionCard.js
@@ -3,91 +3,98 @@ import { daysOfWeek } from "./SessionDayOfWeek";
 import Pills from "@/blocks/Pills";
 import Edit from "@/icons/Edit";
 
+
+const OneTimeSession = ({ sessionData }) => {
+  return (
+      <div className="flex flex-col justify-center w-full p-2 mx-2 my-4 text-center rounded-md md:w-96 lg:w-44 bg-gray-100 text-white bg-opacity-10 dark:border dark:border-info text-sm lg:text-xs">
+        <div className="flex-1 mb-3">
+          <p className="font-semibold tracking-tighter mb-1">{sessionData.title}</p>
+          <p>
+            {sessionData.duration}-hour • ${sessionData.hourly_usd * sessionData.duration} •
+            <span
+              className={`badge ${
+                sessionData.is_live ? "badge-secondary" : "badge-neutral"
+              } badge-sm`}
+            >
+              {sessionData.is_live ? "live" : "draft"}
+            </span>
+            <Edit />
+          </p>
+        </div>
+        <div className="border-t pb-2 dark:border-gray-500" />
+        <h5 className="font-bold">Upcoming &nbsp;
+          {extractDayFromDateTime(sessionData.one_time_date[0])}, {shortDate(sessionData.one_time_date[0])}
+          {sessionData.one_time_date.length > 1 &&
+            <span className="tooltip tooltip-info" data-tip="More available dates">
+                &nbsp;
+                <span className="badge badge-warning dark:badge-info badge-xs font-light text-[0.7em]">+{sessionData.one_time_date.length - 1}</span>
+            </span>
+          }
+        </h5>
+        <div className="flex items-center justify-center py-2 space-x-3 text-xs">
+          <Pills 
+            tags={sessionData.hours.map(val => {
+              if (val < 10) {
+                return `0${val}:00`
+              }
+              return `${val}:00`
+            })}
+            maxWidth="800px" 
+            onClick={() => null} 
+          />
+        </div>
+        <p className="tracking-tighter mb-1">{sessionData.description}</p>
+      </div>
+    );
+}
+
+const RecurringSession = ({ sessionData }) => {
+  return (
+    <div className="flex flex-col justify-center w-full p-2 mx-2 my-4 text-center rounded-md md:w-96 lg:w-44 bg-gray-100 text-white bg-opacity-10 dark:border dark:border-info text-sm lg:text-xs">
+      <div className="flex-1 mb-3">
+        <p className="font-semibold tracking-tighter mb-1">{sessionData.title}</p>
+        <p>
+          {sessionData.duration}-hour • ${sessionData.hourly_usd * sessionData.duration} •
+          <span
+            className={`badge ${
+              sessionData.is_live ? "badge-secondary" : "badge-neutral"
+            } badge-sm`}
+          >
+            {sessionData.is_live ? "live" : "draft"}
+          </span>
+          <Edit />
+        </p>
+      </div>
+      <div className="border-t pb-2 dark:border-gray-500" />
+      <h5 className="font-bold">Every &nbsp;
+        {daysOfWeek[sessionData.day_of_week[0]]}
+        {sessionData.day_of_week.length > 1 &&
+          <span className="tooltip tooltip-info" data-tip="More available days">
+              &nbsp;
+              <span className="badge badge-warning dark:badge-info badge-xs font-light text-[0.7em]">+{sessionData.day_of_week.length - 1}</span>
+          </span>
+        }
+      </h5>
+      <div className="flex items-center justify-center py-2 space-x-3 text-xs">
+        <Pills 
+          tags={sessionData.hours.map(val => {
+            if (val < 10) {
+              return `0${val}:00`
+            }
+            return `${val}:00`
+          })}
+          maxWidth="800px" 
+          onClick={() => null} 
+        />
+      </div>
+      <p className="tracking-tighter mb-1">{sessionData.description}</p>
+    </div>
+  );
+}
 const CurrentSessionCard = ({ data }) => {
-  if (data.day_of_week.length > 0) {
-    let days = data.day_of_week.sort().map(d => daysOfWeek[d]);
-    if (days.length > 3){
-      days = days.splice(0,3);
-      days.push("...")
-    }
-
-    return (
-      <div className="flex flex-col justify-center w-full p-2 mx-2 my-4 text-center rounded-md md:w-96 lg:w-44 bg-gray-100 text-white bg-opacity-10 dark:border dark:border-info text-sm lg:text-xs">
-        <div className="flex-1 mb-3">
-          <p className="font-semibold tracking-tighter mb-1">{data.title}</p>
-          <p>
-            {data.duration}-hour • ${data.hourly_usd * data.duration} •
-            <span
-              className={`badge ${
-                data.is_live ? "badge-secondary" : "badge-neutral"
-              } badge-sm`}
-            >
-              {data.is_live ? "live" : "draft"}
-            </span>
-            <Edit />
-          </p>
-        </div>
-        <div className="border-t pb-2 dark:border-gray-500" />
-        <h5 className="font-bold">Every {days.join(", ")}</h5>
-        <div className="flex items-center justify-center py-2 space-x-3 text-xs">
-          <Pills 
-            tags={data.hours.map(val => {
-              if (val < 10) {
-                return `0${val}:00`
-              }
-              return `${val}:00`
-            })}
-            maxWidth="800px" 
-            onClick={() => null} 
-          />
-        </div>
-        <p className="tracking-tighter mb-1">{data.description}</p>
-      </div>
-    );
-  } else {
-    let dates = data.one_time_date
-      .filter(date => new Date(date) >= new Date())
-      .sort()
-      .map(d => `${extractDayFromDateTime(d)}, ${shortDate(d)}`);
-    if (dates.length > 3){
-      dates = dates.splice(0,3);
-      dates.push("...")
-    } 
-
-    return (
-      <div className="flex flex-col justify-center w-full p-2 mx-2 my-4 text-center rounded-md md:w-96 lg:w-44 bg-gray-100 text-white bg-opacity-10 dark:border dark:border-info text-sm lg:text-xs">
-        <div className="flex-1 mb-3">
-          <p className="font-semibold tracking-tighter mb-1">{data.title}</p>
-          <p>
-            {data.duration}-hour • ${data.hourly_usd * data.duration} •
-            <span
-              className={`badge ${
-                data.is_live ? "badge-secondary" : "badge-neutral"
-              } badge-sm`}
-            >
-              {data.is_live ? "live" : "draft"}
-            </span>
-            <Edit />
-          </p>
-        </div>
-        <div className="border-t pb-2 dark:border-gray-500" />
-        <h5 className="font-bold">Upcoming {dates.join(", ")}</h5>
-        <div className="flex items-center justify-center py-2 space-x-3 text-xs">
-          <Pills 
-            tags={data.hours.map(val => {
-              if (val < 10) {
-                return `0${val}:00`
-              }
-              return `${val}:00`
-            })}
-            maxWidth="800px" 
-            onClick={() => null} 
-          />
-        </div>
-        <p className="tracking-tighter mb-1">{data.description}</p>
-      </div>
-    );
-  }
+  return data.one_time_date.length > 0 ?
+    <OneTimeSession sessionData={data} /> :
+    <RecurringSession sessionData={data} />
 };
 
 export default CurrentSessionCard;
