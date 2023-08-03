6a80778aff8aec4ee83c98c95341e0f8a79d28ca
Vito Ghifari <vitosgh@gmail.com>
2023-08-03 16:56:37 +0700

    fix for build

--- src/components/TextEditor/Toolbar.js
+++ src/components/TextEditor/Toolbar.js
@@ -9,11 +9,9 @@ import {
   UNDO_COMMAND,
 } from "lexical";
 import { insertList } from "@lexical/list";
-import {
-  $createHeadingNode,
-  $createQuoteNode,
-} from '@lexical/rich-text';
+import { $createHeadingNode, $createQuoteNode } from "@lexical/rich-text";
 import { $setBlocksType } from "@lexical/selection";
+import Image from "next/image";
 
 const INSERT_ORDERED_LIST_COMMAND = "insert-ordered-list";
 const INSERT_UNORDERED_LIST_COMMAND = "insert-unordered-list";
@@ -32,7 +30,7 @@ const ToolbarItem = ({ icon, label, onClick, isActive }) => {
       aria-label={label}
       type="button"
     >
-      <img src={icon} alt={label} className="w-4 h-4" />
+      <Image src={icon} alt={label} width={16} height={16} />
     </button>
   );
 };
@@ -43,8 +41,6 @@ const Divider = () => {
   );
 };
 const Toolbar = ({ editable }) => {
-  if (!editable) return;
-
   const [editor] = useLexicalComposerContext();
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
@@ -74,8 +70,8 @@ const Toolbar = ({ editable }) => {
       if ($isRangeSelection(selection)) {
         $setBlocksType(selection, () => $createQuoteNode());
       }
-    })
-  }
+    });
+  };
 
   const createHeadingNode = (h) => {
     editor.update(() => {
@@ -83,84 +79,86 @@ const Toolbar = ({ editable }) => {
       if ($isRangeSelection(selection)) {
         $setBlocksType(selection, () => $createHeadingNode(h));
       }
-    })
-  }
+    });
+  };
 
   return (
-    <div className="flex bg-white border-neutral-200 rounded-sm border">
-      <ToolbarItem
-        icon="/toolbar/undo.svg"
-        label="Undo"
-        onClick={() => editor.dispatchCommand(UNDO_COMMAND)}
-      />
-      <ToolbarItem
-        icon="/toolbar/redo.svg"
-        label="Redo"
-        onClick={() => editor.dispatchCommand(REDO_COMMAND)}
-      />
-      <Divider />
-      <ToolbarItem
-        icon="/toolbar/bold.svg"
-        label="Bold"
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
-          setIsBold(!isBold);
-        }}
-        isActive={isBold}
-      />
-      <ToolbarItem
-        icon="/toolbar/italic.svg"
-        label="Italic"
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
-          setIsItalic(!isItalic);
-        }}
-        isActive={isItalic}
-      />
-      <ToolbarItem
-        icon="/toolbar/underline.svg"
-        label="Underline"
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
-          setIsUnderline(!isUnderline);
-        }}
-        isActive={isUnderline}
-      />
-      <Divider />
-      <ToolbarItem
-        icon="/toolbar/quote.svg"
-        label="Quote"
-        onClick={() => createQuoteNode()}
-      />
-      <ToolbarItem
-        icon="/toolbar/unordered-list.svg"
-        label="Unordered List"
-        onClick={() => editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND)}
-      />
-      <ToolbarItem
-        icon="/toolbar/ordered-list.svg"
-        label="Ordered List"
-        onClick={() => {
-          editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND);
-        }}
-      />
-      <Divider />
-      <ToolbarItem
-        icon="/toolbar/type-h1.svg"
-        label="Heading 1"
-        onClick={() => createHeadingNode("h1")}
-      />
-      <ToolbarItem
-        icon="/toolbar/type-h2.svg"
-        label="Heading 2"
-        onClick={() => createHeadingNode("h2")}
-      />
-      <ToolbarItem
-        icon="/toolbar/type-h3.svg"
-        label="Heading 3"
-        onClick={() => createHeadingNode("h3")}
-      />
-    </div>
+    editable && (
+      <div className="flex bg-white border-neutral-200 rounded-sm border">
+        <ToolbarItem
+          icon="/toolbar/undo.svg"
+          label="Undo"
+          onClick={() => editor.dispatchCommand(UNDO_COMMAND)}
+        />
+        <ToolbarItem
+          icon="/toolbar/redo.svg"
+          label="Redo"
+          onClick={() => editor.dispatchCommand(REDO_COMMAND)}
+        />
+        <Divider />
+        <ToolbarItem
+          icon="/toolbar/bold.svg"
+          label="Bold"
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
+            setIsBold(!isBold);
+          }}
+          isActive={isBold}
+        />
+        <ToolbarItem
+          icon="/toolbar/italic.svg"
+          label="Italic"
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
+            setIsItalic(!isItalic);
+          }}
+          isActive={isItalic}
+        />
+        <ToolbarItem
+          icon="/toolbar/underline.svg"
+          label="Underline"
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
+            setIsUnderline(!isUnderline);
+          }}
+          isActive={isUnderline}
+        />
+        <Divider />
+        <ToolbarItem
+          icon="/toolbar/quote.svg"
+          label="Quote"
+          onClick={() => createQuoteNode()}
+        />
+        <ToolbarItem
+          icon="/toolbar/unordered-list.svg"
+          label="Unordered List"
+          onClick={() => editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND)}
+        />
+        <ToolbarItem
+          icon="/toolbar/ordered-list.svg"
+          label="Ordered List"
+          onClick={() => {
+            editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND);
+          }}
+        />
+        <Divider />
+        <ToolbarItem
+          icon="/toolbar/type-h1.svg"
+          label="Heading 1"
+          onClick={() => createHeadingNode("h1")}
+        />
+        <ToolbarItem
+          icon="/toolbar/type-h2.svg"
+          label="Heading 2"
+          onClick={() => createHeadingNode("h2")}
+        />
+        <ToolbarItem
+          icon="/toolbar/type-h3.svg"
+          label="Heading 3"
+          onClick={() => createHeadingNode("h3")}
+        />
+      </div>
+    )
   );
 };
 
